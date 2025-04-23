89dcabd5d356f973dc23d999fdc164f1e3ef848d
Anuj C More <moreanuj1307@gmail.com>
2025-04-23 13:31:20 -0700

    Cron job: Delete old slots

--- src/cron-jobs/index.ts
+++ src/cron-jobs/index.ts
@@ -35,10 +35,38 @@ export const releaseExpiredOTP = async () => {
       },
     },
   })
-  console.log(`[CRON] Released expired OTPs at ${now.toISOString()}`) }
+  console.log(`[CRON] Released expired OTPs at ${now.toISOString()}`)
+}
+
+export const deleteOldSchedules = async () => {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const yyyy = today.getFullYear()
+  const mm = String(today.getMonth() + 1).padStart(2, '0')
+  const dd = String(today.getDate()).padStart(2, '0')
+  const formattedToday = `${yyyy}-${mm}-${dd}`
+  console.log(today, formattedToday)
+
+  await prisma.schedule.deleteMany({
+    where: {
+      date: {
+        lt: formattedToday,
+      },
+    },
+  })
+
+  console.log(`[CRON] Deleted schedules older than ${formattedToday}`)
+}
 
 // Run every 5 minutes
-cron.schedule('*/5 * * * *', () => {
+cron.schedule('*/1 * * * *', () => {
   releaseExpiredHoldSlots()
   releaseExpiredOTP()
+  // deleteOldSchedules()
+})
+
+// Once a day at 12:00 AM
+cron.schedule('0 0 * * *', () => {
+  deleteOldSchedules()
 })
